3784847497db240ca58e192750ba8f42eaaf4b9d
Woodrow Fulmer <neomagam@gmail.com>
2017-06-29 01:00:44 -0400

    This works
    Well, I tossed out everything he taught us and did it my way. But it
    works.

--- PiFinder.java
+++ PiFinder.java
@@ -1,44 +1,29 @@
 import java.util.concurrent.ThreadLocalRandom;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.ArrayList;
 import java.util.Random;
 
 public class PiFinder {
 
-	public static AtomicInteger hits;
-	public static AtomicInteger total;
-	public static int end;
+	public static AtomicLong hits;
+	//public static AtomicLong total;
+	public static long end;
 	public static int numThreads;
 
 
 	public static void main(String[] args) {
 
-		hits = new AtomicInteger(0);
-		total = new AtomicInteger(0);
-		end = Integer.parseInt(args[0]);
+		hits = new AtomicLong(0);
+		//total = new AtomicLong(0);
+		end = Long.parseLong(args[0]);
 		numThreads = Integer.parseInt(args[1]);
 
-		ArrayList<Thread> threads = new ArrayList<Thread>();
+		Thread[] threads = new Thread[numThreads];
 
 		for (int i = 0; i < numThreads; i++) {
-			threads.add(new Thread(() -> {
-				double x;
-				double y;
-				while (total.get() < end) {	
-					x = ThreadLocalRandom.current().nextDouble();
-					x *= x;
-					y = ThreadLocalRandom.current().nextDouble();
-					y *= y;
-					if (total.get() >= end) {
-						return;
-					} else {
-						if ((x + y) < 1) {
-							hits.getAndIncrement();
-						}
-						total.getAndIncrement();
-					}
-				}
-			}));
+			threads[i] = new Thread(() -> {
+				hits.getAndAdd(getPi(end / numThreads));
+			});
 		}
 
 		for (Thread thread : threads) {
@@ -55,10 +40,30 @@ public class PiFinder {
 		} catch (InterruptedException iex) { }
 
 		System.out.println(hits.get());
-		System.out.println(total.get());
+		System.out.println(end);
 
-		System.out.println(4 * ((double)hits.get() / total.get()));
+		System.out.println(4 * ((float)hits.get() / end));
 
 
+	}
+	
+	public static long getPi(long myEnd) {
+		double x;
+		double y;
+		boolean currComp;
+		long myTotal = 0;
+		long myHits = 0;
+		while (myTotal < myEnd) {	
+			x = ThreadLocalRandom.current().nextDouble();
+			x *= x;
+			y = ThreadLocalRandom.current().nextDouble();
+			y *= y;
+			currComp = (x+y) < 1;
+			if (currComp) {
+				myHits++;
+			}
+			myTotal++;
+		}
+		return myHits;
 	}
 }
